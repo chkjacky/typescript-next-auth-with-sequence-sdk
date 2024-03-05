f60875625c67af316ec807480402aa6cc2767c1f
jackyPersonal <chihunkhaw@gmail.com>
2024-03-05 16:45:16 +0800

    fix(Next): remove server action config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true
-  }
-};
+const nextConfig = {}; // By default server actions are enabled
 
 export default nextConfig;
